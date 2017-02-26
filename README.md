bb9b0605c7e0ca367ed78e4488fceaf3fea64533
Todd Croak-Falen <toddcf@gmail.com>
2017-02-26 00:08:34 -0800

    Pseudo coded CLOCK file.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# wristwatch
+# Timex T53151 Ironman Watch
 Reverse engineering the functions of my wristwatch. Just for fun.
--- assets/js/clock.js
+++ assets/js/clock.js
@@ -1,9 +1,15 @@
 // SET/RECALL button:
+	// HOLD to set date & time. (A whole other series of functions.)
 
 // MODE button:
+	// Goto: CHRONO
 
 // START/SPLIT button:
+	// Press to view Time 1 / Time 2.
+	// HOLD until it toggles permanently to either Time 1 / Time 2.
 
 // STOP/RESET button:
+	// N/A.
 
 // INDIGLO button:
+	// Light.
