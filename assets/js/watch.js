1826a8d68b6a28ff565aada505275929b8afb3b5
Todd Croak-Falen <toddcf@gmail.com>
2017-06-08 20:50:17 -0700

    Initial HTML and CSS structure.

---
+++ assets/js/watch.js
@@ -0,0 +1,17 @@
+// One file, all controllers modularized.
+
+// WATCH CTRL
+
+// UI CTRL
+
+// GLOBAL APP CTRL
+
+	// ctrlSetRecall
+
+	// ctrlMode
+
+	// ctrlStartSplit
+
+	// ctrlStopReset
+
+	// ctrlIndiglo
