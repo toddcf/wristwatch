95634061b5a514569760ce7c2848dd6db9156e56
Todd Croak-Falen <toddcf@gmail.com>
2017-11-24 21:50:37 -0800

    Set all global variables.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,35 @@
 $( document ).ready( function() {
 
-	var beeps = true;
+	// NAVIGATION VARIABLES:
+	var beeps = true; /* Can be set to false to silence beeps. */
+	var next = true;
 
+	// CLOCK VARIABLES:
+	var clock = true;
+	var t1 = true; /* If false, t2 is active. */
+	var t1military = false; /* If true, display T1 in 24-hour mode. */
+	var t2military = false; /* If true, display T2 in 24-hour mode. */
+
+	// CHRONO VARIABLES:
+	var chrono = false;
+	var chronoValue = 000000;
+	var splits = [];
+
+	// TIMER VARIABLES:
+	var timer = false;
+	var timerActive = false;
+	var timerStartPoint = 000000;
+
+	// ALARM VARIABLES:
+	var alarm = false;
+	var alarmArmed = false;
+	var alarmActive = false;
+
+	// INDIGLO VARIABLES:
+	var indiglo = false;
+	var nightMode = false;
+
+	// GLOBAL BUTTON BEHAVIOR:
 	$( "button" ).click( function() {
 
 		if ( beeps ) {
